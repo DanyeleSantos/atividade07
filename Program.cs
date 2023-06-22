2defd5f2f7424cd9cc73331e49327052a7b60abe
DanyeleSantos <135742938+DanyeleSantos@users.noreply.github.com>
2023-06-22 17:08:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula07
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { int valor;
+
+            Console.WriteLine("INSIRA UM VALOR");
+
+             valor = int.Parse(Console.ReadLine());
+
+            if (valor > 1.00)
+            {
+                Console.WriteLine("Valor está correto!");
+            }
+            Console.WriteLine("escolha uma opcao ");
+
+            Console.ReadKey();
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
